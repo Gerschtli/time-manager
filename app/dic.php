92f4e985061f878c2520d19cd2dd57ed84a778c3
Tobias Happ <tobias.happ@gmx.de>
2015-09-22 18:16:23 +0200

    Added dependency pdo

--- app/dic.php
+++ app/dic.php
@@ -1,7 +1,16 @@
 <?php
 
+use Slim\PDO\Database;
 use TimeManager\Decorator\Error as ErrorDecorator;
 
 $app->errorDecorator = function () use ($app) {
     return new ErrorDecorator($app);
 };
+
+$app->container->singleton('pdo', function () use ($app) {
+    $dsn = 'mysql:host=localhost;dbname=time-manager;charset=utf8';
+    $username = 'root';
+    $password = 'root';
+
+    return new Database($dsn, $username, $password);
+});
