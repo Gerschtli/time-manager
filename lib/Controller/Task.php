04f51410c5dca741f04f1ff7e565685445e68fe1
Tobias Happ <tobias.happ@gmx.de>
2015-09-30 20:27:04 +0200

    Added taskController

---
+++ lib/Controller/Task.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TimeManager\Controller;
+
+use Slim\Slim;
+
+class Task
+{
+    private $_app;
+
+    public function __construct(Slim $app) {
+        $this->_app = $app;
+    }
+
+    public function addAction()
+    {
+        
+    }
+}
