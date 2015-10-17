f68a35dcc1aa023780891119d09245f469232f90
Tobias Happ <tobias.happ@gmx.de>
2015-10-17 14:27:35 +0200

    Added model time

---
+++ lib/Model/Time.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace TimeManager\Model;
+
+/**
+ * @Entity @Table(name="tm_time")
+ */
+class Time
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $timeId;
+
+    /**
+     * @ManyToOne(targetEntity="Task", inversedBy="times")
+     * @JoinColumn(name="taskId", referencedColumnName="taskId")
+     * @var \TimeManager\Model\Task
+     */
+    protected $task;
+
+    /**
+     * @Column(type="datetime")
+     * @var \DateTime
+     */
+    protected $start;
+
+    /**
+     * @Column(type="datetime")
+     * @var \DateTime
+     */
+    protected $end;
+
+    public function getTimeId()
+    {
+        return $this->timeId;
+    }
+
+    public function setTask(Task $task)
+    {
+        $this->task = $task;
+    }
+
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    public function setStart(\DateTime $start)
+    {
+        $this->start = $start;
+    }
+
+    public function getStart()
+    {
+        return $this->start;
+    }
+
+    public function setEnd(\DateTime $end)
+    {
+        $this->end = $end;
+    }
+
+    public function getEnd()
+    {
+        return $this->end;
+    }
+}
---
+++ tests/lib/Model/TimeTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace TimeManager\Model;
+
+class TimeTest extends \LocalWebTestCase
+{
+    private $_object;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_object = new Time();
+    }
+
+    public function testGetTimeId()
+    {
+        $timeId             = time();
+        $reflectionClass    = new \ReflectionClass('\TimeManager\Model\Time');
+        $reflectionProperty = $reflectionClass->getProperty('timeId');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($this->_object, $timeId);
+
+        $this->assertEquals($timeId, $this->_object->getTimeId());
+    }
+
+    /**
+     * @dataProvider dataProviderForTestGetterSetter
+     */
+    public function testGetterSetter($name, $value)
+    {
+        $setter = 'set' . ucfirst($name);
+        $getter = 'get' . ucfirst($name);
+
+        $this->_object->$setter($value);
+        $this->assertEquals($value, $this->_object->$getter());
+    }
+
+    public function dataProviderForTestGetterSetter()
+    {
+        $task = $this->getMock('\TimeManager\Model\Task');
+        $datetime = $this->getMock('\DateTime');
+
+        return [
+            ['task', $task],
+            ['start', $datetime],
+            ['end', $datetime],
+        ];
+    }
+}
