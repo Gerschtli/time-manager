fcd4cb5f2f97946fcafbc1c7a0f8e29351ba4466
Tobias Happ <tobias.happ@gmx.de>
2015-10-17 02:41:34 +0200

    Renamed id columns

--- manifests/resources/tables.sql
+++ manifests/resources/tables.sql
@@ -1,24 +1,24 @@
 USE `time-manager`;
 
 CREATE TABLE `projects` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `projectId` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(127) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`projectId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `taskId` int(11) NOT NULL AUTO_INCREMENT,
   `projectId` int(11) NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`)
+  PRIMARY KEY (`taskId`),
+  FOREIGN KEY (`projectId`) REFERENCES `projects` (`projectId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `time` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timeId` int(11) NOT NULL AUTO_INCREMENT,
   `taskId` int(11) NOT NULL,
   `start` datetime NOT NULL,
   `end` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`taskId`) REFERENCES `tasks` (`id`)
+  PRIMARY KEY (`timeId`),
+  FOREIGN KEY (`taskId`) REFERENCES `tasks` (`taskId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
