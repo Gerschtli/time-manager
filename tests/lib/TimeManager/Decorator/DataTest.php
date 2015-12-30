6dba7eb45f1ed88d0b0862ab502990f849a732f6
Tobias Happ <tobias.happ@gmx.de>
2015-12-30 23:02:28 +0100

    Added test

--- tests/lib/TimeManager/Decorator/DataTest.php
+++ tests/lib/TimeManager/Decorator/DataTest.php
@@ -78,4 +78,51 @@ class DataTest extends \LocalWebTestCase
         $this->assertEquals('application/json', $response->headers->get('Content-Type'));
         $this->assertJsonStringEqualsJsonString($body, $response->getBody());
     }
+
+    public function testProcessWithArrayMessage()
+    {
+        $task = new Task();
+        $task->taskId      = 5;
+        $task->description = 'description';
+        $task->project     = (object)[
+            'name' => 'project',
+        ];
+        $task->times       = new ArrayCollection(
+            [
+                (object)[
+                    'start' => new DateTime('2015-10-10'),
+                ],
+                (object)[
+                    'start' => new DateTime('2015-10-10'),
+                    'end'   => new DateTime('2015-11-11'),
+                ],
+            ]
+        );
+
+        $body = json_encode(
+            [
+                (object)[
+                    'taskId'      => 5,
+                    'description' => 'description',
+                    'project'     => 'project',
+                    'times'       => [
+                        (object)[
+                            'start' => '2015-10-10 00:00:00',
+                        ],
+                        (object)[
+                            'start' => '2015-10-10 00:00:00',
+                            'end'   => '2015-11-11 00:00:00',
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        $this->_object->process(200, [$task]);
+
+        $response = $this->app->response;
+        $this->assertEquals(200, $response->getStatus());
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        $this->assertJsonStringEqualsJsonString($body, $response->getBody());
+    }
 }
