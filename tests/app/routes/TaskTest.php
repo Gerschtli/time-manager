c56fd25737844dbbcc17722fc865a4d4ccfea4b7
Tobias Happ <tobias.happ@gmx.de>
2015-12-30 17:09:22 +0100

    Modified test for taskId

--- tests/app/routes/TaskTest.php
+++ tests/app/routes/TaskTest.php
@@ -19,14 +19,17 @@ class TaskTest extends \LocalWebTestCase
 
     public function testGetAction()
     {
+        $taskId = time();
+
         $this->app->controllerTask = $this
             ->getMockBuilder('\TimeManager\Controller\Task')
             ->disableOriginalConstructor()
             ->getMock();
         $this->app->controllerTask
             ->expects($this->once())
-            ->method('getAction');
+            ->method('getAction')
+            ->with($this->equalTo($taskId));
 
-        $this->client->get('/task/2');
+        $this->client->get('/task/' . $taskId);
     }
 }
