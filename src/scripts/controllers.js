ba749120a5576ea28a9f42dd968b4864383739d6
Tobias Happ <tobias.happ@gmx.de>
2016-07-02 16:07:36 +0200

    Used toastr for event logging

--- src/scripts/controllers.js
+++ src/scripts/controllers.js
@@ -5,12 +5,11 @@
         .module('timeManager')
         .controller('MainController', MainController);
 
-    MainController.$inject = ['$http'];
+    MainController.$inject = ['$http', 'toastr'];
 
     /* @ngInject */
-    function MainController($http) {
+    function MainController($http, toastr) {
         var vm = this;
-        vm.title = 'MainController';
         vm.tasks = [];
         vm.createTask = createTask;
         vm.updateTask = updateTask;
@@ -40,22 +39,27 @@
 
         function createTask(task) {
             // TODO
+            toastr.info('Create Task');
         }
 
         function updateTask(task) {
             // TODO
+            toastr.info('Update Task');
         }
 
         function deleteTask(task) {
             // TODO
+            toastr.info('Delete Task');
         }
 
         function addTime(task, time) {
             // TODO
+            toastr.info('Add Time');
         }
 
         function deleteTime(task, time) {
             // TODO
+            toastr.info('Delete Time');
         }
     }
 })();
