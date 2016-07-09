bfd46af76da5a8485831b1805e90492d8c689fe4
Tobias Happ <tobias.happ@gmx.de>
2016-07-10 01:37:43 +0200

    Removed unused code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,9 +5,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 date_default_timezone_set('UTC');
 
-use Slim\Slim;
-use There4\Slim\Test\WebTestCase;
-
 if (!defined('PROJECT_ROOT')) {
     define('PROJECT_ROOT', realpath(__DIR__ . '/..'));
 }
@@ -19,18 +16,3 @@ if (!defined('APPLICATION_ENV')) {
 }
 
 require_once(PROJECT_ROOT . '/vendor/autoload.php');
-
-class LocalWebTestCase extends WebTestCase
-{
-    public function getSlimInstance()
-    {
-        $app = new Slim(
-            [
-                'debug' => false,
-            ]
-        );
-
-        require(PROJECT_ROOT . '/app/app.php');
-        return $app;
-    }
-}
