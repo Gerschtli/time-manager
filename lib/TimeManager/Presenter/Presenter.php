74ab5afd6ac6e6437fc30c5b4a4d81ce8a1b2102
Tobias Happ <tobias.happ@gmx.de>
2016-07-30 14:55:38 +0200

    Removed unused constants

--- lib/TimeManager/Presenter/Presenter.php
+++ lib/TimeManager/Presenter/Presenter.php
@@ -9,21 +9,12 @@ abstract class Presenter
     const STATUS_OK                     = 200;
     const STATUS_CREATED                = 201;
     const STATUS_ACCEPTED               = 202;
-    const STATUS_BAD_REQUEST            = 400;
     const STATUS_NOT_FOUND              = 404;
-    const STATUS_UNSUPPORTED_MEDIA_TYPE = 415;
     const STATUS_UNPROCESSABLE_ENTITY   = 422;
     const STATUS_INTERNAL_SERVER_ERROR  = 500;
 
     const DESCRIPTION_SUCCESSFUL_DELETION = 'Deletion successful';
     const DESCRIPTION_INVALID_STRUCTURE   = 'JSON is in invalid data structure';
     const DESCRIPTION_NONEXISTING_KEY     = 'No Data with provided Key found';
-    const DESCRIPTION_ONLY_JSON           = 'Only JSON is allowed';
-    const DESCRIPTION_PARSE_ERROR         = 'JSON Parse Error';
     const DESCRIPTION_NO_ROUTE_MATCHED    = 'No existing Route matched';
-
-    protected function _print(Response $response, $code, $body)
-    {
-        return $response->withJson($body, $code);
-    }
 }
--- tests/lib/TimeManager/Presenter/PresenterTest.php
+++ tests/lib/TimeManager/Presenter/PresenterTest.php
@@ -8,23 +8,19 @@ class PresenterTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstants()
     {
-        $object = new ReflectionClass('\TimeManager\Presenter\Presenter');
+        $object = new ReflectionClass(Presenter::class);
 
         $this->assertEquals(
             [
                 'STATUS_OK'                       => 200,
                 'STATUS_CREATED'                  => 201,
                 'STATUS_ACCEPTED'                 => 202,
-                'STATUS_BAD_REQUEST'              => 400,
                 'STATUS_NOT_FOUND'                => 404,
-                'STATUS_UNSUPPORTED_MEDIA_TYPE'   => 415,
                 'STATUS_UNPROCESSABLE_ENTITY'     => 422,
                 'STATUS_INTERNAL_SERVER_ERROR'    => 500,
                 'DESCRIPTION_SUCCESSFUL_DELETION' => 'Deletion successful',
                 'DESCRIPTION_INVALID_STRUCTURE'   => 'JSON is in invalid data structure',
                 'DESCRIPTION_NONEXISTING_KEY'     => 'No Data with provided Key found',
-                'DESCRIPTION_ONLY_JSON'           => 'Only JSON is allowed',
-                'DESCRIPTION_PARSE_ERROR'         => 'JSON Parse Error',
                 'DESCRIPTION_NO_ROUTE_MATCHED'    => 'No existing Route matched',
             ],
             $object->getConstants()
