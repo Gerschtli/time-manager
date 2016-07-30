e40fb38bf56624b758ad2b41c8ce3c9fad524b89
Tobias Happ <tobias.happ@gmx.de>
2016-07-30 15:29:03 +0200

    Added new constants for method not allowed

--- lib/TimeManager/Presenter/Presenter.php
+++ lib/TimeManager/Presenter/Presenter.php
@@ -8,6 +8,7 @@ abstract class Presenter
     const STATUS_CREATED                = 201;
     const STATUS_ACCEPTED               = 202;
     const STATUS_NOT_FOUND              = 404;
+    const STATUS_METHOD_NOT_ALLOWED     = 405;
     const STATUS_UNPROCESSABLE_ENTITY   = 422;
     const STATUS_INTERNAL_SERVER_ERROR  = 500;
 
@@ -15,4 +16,5 @@ abstract class Presenter
     const DESCRIPTION_INVALID_STRUCTURE   = 'JSON is in invalid data structure';
     const DESCRIPTION_NONEXISTING_KEY     = 'No Data with provided Key found';
     const DESCRIPTION_NO_ROUTE_MATCHED    = 'No existing Route matched';
+    const DESCRIPTION_AVAILABLE_METHODS   = 'Available Methods: %s';
 }
--- tests/lib/TimeManager/Presenter/PresenterTest.php
+++ tests/lib/TimeManager/Presenter/PresenterTest.php
@@ -16,12 +16,14 @@ class PresenterTest extends \PHPUnit_Framework_TestCase
                 'STATUS_CREATED'                  => 201,
                 'STATUS_ACCEPTED'                 => 202,
                 'STATUS_NOT_FOUND'                => 404,
+                'STATUS_METHOD_NOT_ALLOWED'       => 405,
                 'STATUS_UNPROCESSABLE_ENTITY'     => 422,
                 'STATUS_INTERNAL_SERVER_ERROR'    => 500,
                 'DESCRIPTION_SUCCESSFUL_DELETION' => 'Deletion successful',
                 'DESCRIPTION_INVALID_STRUCTURE'   => 'JSON is in invalid data structure',
                 'DESCRIPTION_NONEXISTING_KEY'     => 'No Data with provided Key found',
                 'DESCRIPTION_NO_ROUTE_MATCHED'    => 'No existing Route matched',
+                'DESCRIPTION_AVAILABLE_METHODS'   => 'Available Methods: %s',
             ],
             $object->getConstants()
         );
