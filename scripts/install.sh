c0e421a9aae8958f7a0f07393baa47d9de2a6ff6
Tobias Happ <tobias.happ@gmx.de>
2016-08-14 16:32:44 +0200

    Updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname $(readlink -f "${0}"))"
-UPDATE_CONFIG="$(dirname ${SCRIPT_DIR})/update-config.sh"
-WORKSPACE_DIR="${SCRIPT_DIR}/workspace"
-LIVE_DIR="${SCRIPT_DIR}/live"
+SCRIPTPATH="$(dirname $(readlink -f "${0}"))"
 
-GIT_DIR="${SCRIPT_DIR}/project"
-GIT_URL="git@github.com:Gerschtli/time-manager.git"
-GIT_BRANCH="master"
-
-if [[ "${APPLICATION_ENV}" == "development" ]]; then
-    GIT_DIR="$(dirname ${SCRIPT_DIR})"
+if [ -r "${SCRIPTPATH}/install.conf" ]; then
+    source "${SCRIPTPATH}/install.conf"
 fi
 
-_log(){
-    if [[ "${2}" == "ERROR" ]]; then
+GIT_URL="${GIT_URL:-git@github.com:Gerschtli/time-manager.git}"
+GIT_DIR="${GIT_DIR:-${SCRIPTPATH}/project}"
+GIT_BRANCH="${GIT_BRANCH:-master}"
+
+WORK_DIR="${WORK_DIR:-${SCRIPTPATH}}"
+WORKSPACE_DIR="${SCRIPTPATH}/workspace"
+SHARED_DIR="${SCRIPTPATH}/shared"
+INSTALL_DIR="${INSTALL_DIR:-/var/www/time-manager}"
+
+BUILD=$(date '+%s')
+MAX_BACKUPS=10
+
+_log() {
+    if [ "${2}" = "ERROR" ]; then
         echo -en "\033[31m"
     else
         echo -en "\033[32m"
@@ -24,57 +29,32 @@ _log(){
 }
 export -f _log
 
-_vagrant() {
-    pushd "${GIT_DIR}" 1> /dev/null
-
-    vagrant status | grep "running (virtualbox)" >> /dev/null
-    if [[ "${?}" != 0 ]]; then
-        _log "vagrant up ..."
-        vagrant up --provision
-    else
-        _log "vagrant provision ..."
-        vagrant provision
-    fi
-
-    popd 1> /dev/null
-}
-
-_export(){
-    if [[ ! -d "${GIT_DIR}" ]]; then
+_export() {
+    if [ ! -d "${GIT_DIR}" ];then
         _log "cloning the repository ..."
         git clone "${GIT_URL}" "${GIT_DIR}"
     fi
-
     pushd "${GIT_DIR}" 1> /dev/null
-    _log "get latest code base [git pull origin ${GIT_BRANCH}] ..."
+    _log "get latest code base [git pull origin ${GIT_BRANCH}]..."
     git fetch --prune
-    git checkout -b "${GIT_BRANCH}"
-    git checkout "${GIT_BRANCH}"
-    git pull origin "${GIT_BRANCH}"
+    git checkout -f ${GIT_BRANCH}
+    git pull origin ${GIT_BRANCH}
     popd 1> /dev/null
 }
 
 _composer() {
+    pushd "${WORK_DIR}"
+    if [ ! -f "${WORK_DIR}/composer.phar" ];then
+        _log "Downloading composer ...."
+        curl -sS "https://getcomposer.org/installer" | php
+    else
+        _log "Composer update...."
+        php composer.phar self-update
+    fi
+    popd 1> /dev/null
+
     pushd "${GIT_DIR}" 1> /dev/null
-
-    COMPOSER="./bin/composer.phar"
-    if [[ ! -x "${COMPOSER}" ]]; then
-        _log "download composer ..."
-        mkdir -p bin
-        curl -sS https://getcomposer.org/installer | php -- --install-dir=bin
-    else
-        _log "update composer ..."
-        "${COMPOSER}" self-update
-    fi
-
-    _log "install composer dependencies ..."
-    if [[ "${1}" == "dev" ]]; then
-        "${COMPOSER}" install --prefer-source
-    else
-        "${COMPOSER}" install --prefer-source --no-dev
-    fi
-
-
+    php "${WORK_DIR}/composer.phar" install --no-dev
     popd 1> /dev/null
 }
 
@@ -84,17 +64,17 @@ _gulp() {
     npm install
 
     _log "build gulp ..."
-    ./node_modules/gulp/bin/gulp.js build
+    ./node_modules/gulp/bin/gulp.js build --production
     popd 1> /dev/null
 }
 
 _build() {
-    if [[ -d "${WORKSPACE_DIR}" ]]; then
+    if [ -d "${WORKSPACE_DIR}" ];then
         _log "remove old workspace ... "
         rm -R "${WORKSPACE_DIR}"
     fi
     _log "creating workspace ... "
-    mkdir -p "${WORKSPACE_DIR}"
+    mkdir -p ${WORKSPACE_DIR}
 
     _log "install code ..."
     pushd "${GIT_DIR}" 1> /dev/null
@@ -102,89 +82,78 @@ _build() {
     popd 1> /dev/null
 }
 
+_updateConfig() {
+    if [[ -f "${SHARED_DIR}/parameter.ini" ]]; then
+        _log "update config ..."
+        cp "${SHARED_DIR}/parameter.ini" "${WORKSPACE_DIR}/app"
+    else
+        _log "parameter.ini does not exist!" "ERROR"
+    fi
+}
+
 _cleanSource() {
+    _log 'cleaning workspace ...'
     pushd "${WORKSPACE_DIR}" 1> /dev/null
-    _log "cleaning workspace ..."
     find . -name ".git" -exec rm -Rf {} \+
     find . -maxdepth 1 ! -name "." \
         -and ! -name ".." \
         -and ! -name "app" \
-        -and ! -name "lib" \
         -and ! -name "dist" \
+        -and ! -name "lib" \
         -and ! -name "vendor" \
         -exec rm -Rf {} \+
     popd 1> /dev/null
 }
 
-_updateConfig() {
-    _log "update config ..."
-    "${UPDATE_CONFIG}" "${WORKSPACE_DIR}/app/config.php"
+_move() {
+    _log 'moving the source to ...'
+    mv "${WORKSPACE_DIR}" "${INSTALL_DIR}/BUILD-${BUILD}"
 }
 
-_compareFolders() {
-    TMP="/tmp/time-manager"
+_link() {
+    _log 'create a symbolic link ...'
+    ln -snf "${INSTALL_DIR}/BUILD-${BUILD}" "${INSTALL_DIR}/current"
+}
 
-    pushd "${SCRIPT_DIR}" 1> /dev/null
-    _log "compare folders ..."
+_reloadApache() {
+    _log 'Reload Apache ...'
+    service apache2 reload
+}
 
-    if [[ ! -d "${LIVE_DIR}" ]]; then
-        _log "create live dir ... "
-        mkdir -p "${LIVE_DIR}"
-    fi
-
-    diff -arq "${WORKSPACE_DIR}" "${LIVE_DIR}" \
-        | grep "Only in ${LIVE_DIR}" \
-        | sed "s,:\ ,/," \
-        | sed "s,Only in ${LIVE_DIR},\t," > "${TMP}"
-
-    if [[ "$?" == 0 ]]; then
-        _log "files/folders to remove:" "ERROR"
-        cat "${TMP}"
-    fi
-
+_cleanup() {
+    _log 'cleanup ...'
+    pushd ${INSTALL_DIR} 1> /dev/null
+    _log 'packing previous build(s) ...'
+    BUILDS=( $(find . -maxdepth 1 -type d -name "BUILD-*" -printf "%f\n" | sort -r ) )
+    for B in "${!BUILDS[@]}"; do
+        if [ "${BUILDS[${B}]}" != "BUILD-${BUILD}" ]; then
+            tar -jcpf ${BUILDS[${B}]}.tar.bz2 ${BUILDS[${B}]} --exclude='logs/*'
+            rm -Rf ${INSTALL_DIR}/${BUILDS[${B}]}
+        fi
+    done
+    _log 'deleting old backup files ...'
+    BUILDS=( $(find . -maxdepth 1 -type f -name "BUILD-*.tar.bz2" | sort -r ) )
+    for B in "${!BUILDS[@]}"; do
+        if [ ${B} -ge ${MAX_BACKUPS} ]; then
+            rm -Rf ${INSTALL_DIR}/${BUILDS[${B}]}
+        fi
+    done
     popd 1> /dev/null
 }
 
-_copyWorkspace() {
-    read -p "copy workspace into live? [y/n] " CHOICE
-
-    if [[ "${CHOICE}" == "y" ]]; then
-        _log "remove live dir ..."
-        rm -rf "${LIVE_DIR}"
-        mkdir -p "${LIVE_DIR}"
-
-        _log "copy workspace ..."
-        pushd "${WORKSPACE_DIR}" 1> /dev/null
-        cp -R * "${LIVE_DIR}"
-        popd 1> /dev/null
-    fi
-}
-
-
-_development() {
-    _log "development build ..."
-    _vagrant
-    _composer "dev"
-    _gulp
-}
-
-_production() {
-    _log "production build ..."
+_main() {
     _export
-    _composer "no-dev"
+    _composer
     _gulp
     _build
-    _cleanSource
     _updateConfig
-    _compareFolders
-    _copyWorkspace
+    _cleanSource
+    _move
+    _link
+    _reloadApache
+    _cleanup
 }
- 
 
-if [[ "${APPLICATION_ENV}" == "production" ]]; then
-    _production
-else
-    _development
-fi
+_main
 
 exit 0
